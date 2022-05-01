2c1c74f4d7fdc3fd604d961e5e7073974ee50bdb
Michał Sidor <public+git@meekchopp.es>
2022-05-01 18:00:12 +0200

    add a bit of automation

---
+++ add_missing.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import datetime
+import json
+from pathlib import Path
+
+with Path("data/oc.json").open() as f:
+    data: list = json.load(f)
+
+existing = {x["id"] for x in data}
+
+for filename in Path("assets").glob("*.png"):
+    ident = int(filename.stem)
+    if ident not in existing:
+        data.insert(0, {"id": ident, "dt": datetime.datetime.now().astimezone().isoformat()})
+
+with Path("data/oc.json.new").open("w") as f:
+    json.dump(data, f, indent=2)
+
+Path("data/oc.json.new").rename("data/oc.json")
